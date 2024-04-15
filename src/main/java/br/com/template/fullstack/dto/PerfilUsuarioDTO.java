7e8502e6d30040d7ea8a05c338f1fd52192468cb
JerfesonGregorio <pro.jerfeson@gmail.com>
2024-04-15 16:40:48 -0300

    implementando crud de perfil-usuario

---
+++ src/main/java/br/com/template/fullstack/dto/PerfilUsuarioDTO.java
@@ -0,0 +1,29 @@
+package br.com.template.fullstack.dto;
+
+import br.com.template.fullstack.entity.PerfilUsuarioEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PerfilUsuarioDTO {
+
+    private Long id;
+    private UsuarioDTO usuario;
+    private PerfilDTO perfil;
+
+    public PerfilUsuarioDTO(PerfilUsuarioEntity perfilUsuario) {
+        BeanUtils.copyProperties(perfilUsuario, this);
+
+        if(perfilUsuario != null && perfilUsuario.getUsuario() != null) {
+            this.usuario = new UsuarioDTO(perfilUsuario.getUsuario());
+        }
+
+        if(perfilUsuario != null && perfilUsuario.getUsuario() != null) {
+            this.perfil = new PerfilDTO(perfilUsuario.getPerfil());
+        }
+    }
+}
---
+++ src/main/java/br/com/template/fullstack/entity/PerfilUsuarioEntity.java
@@ -0,0 +1,43 @@
+package br.com.template.fullstack.entity;
+
+import br.com.template.fullstack.dto.PerfilDTO;
+import br.com.template.fullstack.dto.PerfilUsuarioDTO;
+import br.com.template.fullstack.dto.UsuarioDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TESTE_PERFIL_USUARIO")
+@Getter
+@Setter
+@NoArgsConstructor
+public class PerfilUsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_USUARIO")
+    private UsuarioEntity usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_PERFIL")
+    private PerfilEntity perfil;
+
+    public PerfilUsuarioEntity(PerfilUsuarioDTO perfilUsuario) {
+
+        if(perfilUsuario != null && perfilUsuario.getUsuario() != null) {
+            this.usuario = new UsuarioEntity(perfilUsuario.getUsuario());
+        }
+
+        if(perfilUsuario != null && perfilUsuario.getUsuario() != null) {
+            this.perfil = new PerfilEntity(perfilUsuario.getPerfil());
+        }
+
+    }
+
+
+}
